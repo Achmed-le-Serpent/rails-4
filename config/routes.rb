15ea3a4dadd8b40dd1924afba992e0fbed8c2309
Borgne <ronan.borgne@laposte.net>
2017-11-07 23:56:47 +0100

    git

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :moussaillons
+
+	devise_for :moussaillons
+
+	root 'moussaillons#index'
+
+	resources :moussaillons
+
+	resources :gossips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
